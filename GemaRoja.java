206f1f5b86db66598479c0dd28da95ef1cc98429
DL123-a <dannylealgt@gmail.com>
2025-05-27 19:33:54 -0600

    Delete GemaRoja.java

--- GemaRoja.java
+++
@@ -1,9 +0,0 @@
-import greenfoot.*;
-
-public class GemaRoja extends Actor {
-    public GemaRoja() {
-        GreenfootImage img = new GreenfootImage("gema_roja.png");
-        img.scale(32, 32);
-        setImage(img);
-    }
-}
